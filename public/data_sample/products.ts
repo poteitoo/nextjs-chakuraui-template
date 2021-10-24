0aa8a517cfbf31a8917bf45f1c94dd2b7fc6e20e
poteitoo <1105tigery@gmail.com>
2021-10-24 18:33:36 +0900

    [Update] data

---
+++ public/data_sample/products.ts
@@ -0,0 +1,32 @@
+export const data = {
+  products: [
+    {
+      id: "1",
+      name: "Animal",
+      currency: "USD",
+      price: 39.99,
+      imageUrl: "https://picsum.photos/id/1003/400/600",
+    },
+    {
+      id: "2",
+      name: "Bird",
+      currency: "USD",
+      price: 199.99,
+      imageUrl: "https://picsum.photos/id/1024/400/600",
+    },
+    {
+      id: "3",
+      name: "Cold Dog",
+      currency: "USD",
+      price: 299.99,
+      imageUrl: "https://picsum.photos/id/1025/400/600",
+    },
+    {
+      id: "4",
+      name: "Bear",
+      currency: "USD",
+      price: 499.99,
+      imageUrl: "https://picsum.photos/id/1020/400/600",
+    },
+  ],
+};
